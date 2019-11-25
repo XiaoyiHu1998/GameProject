bf234af69937563a26f9addc5a07a2f933bebd8f
Azat <azatsa@gmail.com>
2019-11-25 23:38:37 +0100

    Added package for rabbit

---
+++ UU_GameProject_2019/Assets/RabbitMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RabbitMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private string[] m_buttonNames = new string[] { "Idle", "Run", "Dead" };
+
+    private Animator m_animator;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        m_animator = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_animator.SetInteger("AnimIndex", 0);
+        m_animator.SetTrigger("Next");
+    }
+
+
+}
