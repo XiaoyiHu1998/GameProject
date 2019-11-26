36b9406d567867551d6f266dfc335a7df879b4d2
Xiao yi Hu <xiaoyi.hu@student.hu.nl>
2019-11-26 21:49:45 +0100

    added character color selection

---
+++ UU_GameProject_2019/Assets/Scripts/EthanBody_1_Color.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EthanBody_1_Color : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(UnityEngine.Input.GetKeyDown(KeyCode.Z))
+        {
+            GetComponent<Renderer>().enabled = false;
+        }
+        else if(UnityEngine.Input.GetKeyDown(KeyCode.X))
+        {
+            GetComponent<Renderer>().enabled = true;
+        }
+    }
+}
---
+++ UU_GameProject_2019/Assets/Scripts/EthanBody_Color.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EthanBody_Color : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(UnityEngine.Input.GetKeyDown(KeyCode.Z))
+        {
+            GetComponent<Renderer>().enabled = true;
+        }
+        else if(UnityEngine.Input.GetKeyDown(KeyCode.X))
+        {
+            GetComponent<Renderer>().enabled = false;
+        }
+    }
+}
