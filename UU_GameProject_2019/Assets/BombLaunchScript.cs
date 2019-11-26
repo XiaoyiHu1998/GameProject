f9c09e60f886f32170a3838754c495d6ac1c93c2
Asehujiko <asehujiko_@hotmail.com>
2019-11-26 02:27:53 +0100

    bombs
    bombs

---
+++ UU_GameProject_2019/Assets/BombLaunchScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BombLaunchScript : MonoBehaviour
+{
+    public GameObject BombEmitter;
+    public GameObject Bomb;
+
+    public float BombFuseTime;
+    public Vector3 BombForce;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            GameObject MyBomb = Instantiate(Bomb, BombEmitter.transform.position, BombEmitter.transform.rotation) as GameObject;
+            MyBomb.GetComponent<Rigidbody>().AddRelativeForce(BombForce);
+            Destroy(MyBomb, BombFuseTime);
+        }
+    }
+}
