fcb51f7c11e7d4856bf74d363b44fda4998bd5a9
Xiao yi Hu <xiaoyi.hu@student.hu.nl>
2019-11-28 15:39:19 +0100

    sword now only becomes visible after pressing E, but does not become invisible again

--- UU_GameProject_2019/Assets/Scripts/SwordSwing.cs
+++ UU_GameProject_2019/Assets/Scripts/SwordSwing.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 public class SwordSwing : MonoBehaviour
 {
+    private Animation animation;
     // Start is called before the first frame update
     void Start()
     {
-        
+            GetComponent<Renderer>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKey(KeyCode.E))
+        if(Input.GetKey(KeyCode.E)){
+            GetComponent<Renderer>().enabled = true;
+            animation.Play("swordSwing");
+            GetComponent<Renderer>().enabled = false;
+            
+        }
     }
 }
