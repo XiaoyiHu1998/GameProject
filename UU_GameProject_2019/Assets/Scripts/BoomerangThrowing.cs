5c21c1c087c53f97d523a68cad9402884db7f6ca
Stefan <stefan13401@outlook.com>
2019-11-29 21:52:19 +0100

    added boomerang throwing, but it doesn't return yet

---
+++ UU_GameProject_2019/Assets/Scripts/BoomerangThrowing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoomerangThrowing : MonoBehaviour
+{
+    public GameObject BoomerangEmitter;
+    public GameObject BoomerangPrefab;
+    public Vector3 ThrowingForce;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GameObject Boomerang = Instantiate(BoomerangPrefab, transform.position, transform.rotation) as GameObject;
+            Rigidbody rb = Boomerang.GetComponent<Rigidbody>();
+            rb.AddForce(ThrowingForce);
+        }
+    }
+}
