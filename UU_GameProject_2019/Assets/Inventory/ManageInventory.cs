6cb3c6c5cacd9a38739d1beb878ab6254aca5c4b
Drawoon <57985979+Drawoon@users.noreply.github.com>
2019-12-05 14:48:08 +0100

    changed to 4 items
    it used to be 5

--- UU_GameProject_2019/Assets/Inventory/ManageInventory.cs
+++ UU_GameProject_2019/Assets/Inventory/ManageInventory.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ManageInventory : MonoBehaviour
 {
-    public GameObject Item1, Item2, Item3, Item4, Item5;
+    public GameObject Item1, Item2, Item3, Item4;
     public GameObject Cursor;
     public int Selection { get; private set; }
 
@@ -15,22 +15,26 @@ public class ManageInventory : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown("e")) SwitchItem(Selection+1);
-        if (Input.GetKeyDown("q")) SwitchItem(Selection-1);
+        if (Input.GetKeyDown("e")) SwitchItem(Selection + 1);
+        if (Input.GetKeyDown("q")) SwitchItem(Selection - 1);
+        if (Input.GetKeyDown("1")) SwitchItem(0);
+        if (Input.GetKeyDown("2")) SwitchItem(1);
+        if (Input.GetKeyDown("3")) SwitchItem(2);
+        if (Input.GetKeyDown("4")) SwitchItem(3);
     }
 
     public void SwitchItem(int switchto)
     {
-        if (switchto >= 0 && switchto <= 4)
+        if (switchto >= 0 && switchto <= 3)
         {
             Selection = switchto;
-            GameObject[] ItemList = new GameObject[] { Item1, Item2, Item3, Item4, Item5 };
+            GameObject[] ItemList = new GameObject[] { Item1, Item2, Item3, Item4 };
             foreach (GameObject o in ItemList)
             {
                 o.SetActive(false);
             }
             ItemList[Selection].SetActive(true);
-            Cursor.transform.localPosition = new Vector3(0, 200 - 100*Selection, 0);
+            Cursor.transform.localPosition = new Vector3(0, 150 - 100*Selection, 0);
         }
     }
 }
