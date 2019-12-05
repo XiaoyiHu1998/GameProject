15f53fc4a0d2f96c200147a0681a715bf2f3b493
Stefan <stefan13401@outlook.com>
2019-12-05 15:00:46 +0100

    current boomerang with returning

---
+++ UU_GameProject_2019/Assets/Scripts/BoomerangMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoomerangMovement : MonoBehaviour
+{
+    public float delay = 0.2f;
+    Vector3 velocity;
+    float maxVelocity;
+    public float returnRate = 0.5f;
+    public GameObject Player;
+    bool returning = false;
+
+    void Start()
+    {
+        Invoke("wait", delay);
+    }
+
+    void wait()
+    {
+        returning = true;
+    }
+
+    void Update()
+    {
+        if (returning)
+        {
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.velocity = rb.velocity + (Player.transform.position - transform.position).normalized * returnRate;
+        }
+    }
+
+    /*
+    void Update()
+    {
+        transform.position += vel; 
+        if (delay > 0) 
+            delay -= Time.deltaTime;
+        else
+        {
+            vel = vel + (followThis.transfom.position - transform.position).normalized * Time.deltaTime * returnRate);
+            vel = ve.normalized * Mathf.Clamp(vel.magnitude, 0, maxVel);
+        }
+    }*/
+}
