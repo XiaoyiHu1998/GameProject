40128385604bc3e192e70bbb622931c32d58746e
Stefan <stefan13401@outlook.com>
2019-12-07 20:10:25 +0100

    Added Movement and animation
    Also Added the whole enemy etc.

--- UU_GameProject_2019/Assets/Scripts/MetalonMovement.cs
+++ UU_GameProject_2019/Assets/Scripts/MetalonMovement.cs
@@ -2,31 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RabbitMovement : MonoBehaviour
+public class MetalonMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-
-    private string[] m_buttonNames = new string[] { "Idle", "Run", "Dead" };
-
     private Animator m_animator;
-
     protected Vector2 zRange, xRange;
     protected Vector3 targetPosition;
     bool moving;
-
     float idleTime, speed, timer;
-    // Use this for initialization
+
+    //Temporary viuals
+    public float AngleDifference;
+
+    // Start is called before the first frame update
     void Start()
     {
-        zRange = new Vector2(10, 18);
-        xRange = new Vector2(13, 28);
+        zRange = new Vector2(10, 23);
+        xRange = new Vector2(13, 36);
         idleTime = 4f;
         speed = 1;
         moving = true;
         m_animator = GetComponent<Animator>();
         SetNewTargetPosition();
-        m_animator.SetInteger("AnimIndex", 1);
-        m_animator.SetTrigger("Next");
+        m_animator.SetBool("Walk Forward", true);
     }
 
     // Update is called once per frame
@@ -40,8 +37,7 @@ public class RabbitMovement : MonoBehaviour
         if (!moving)
         {
             timer += Time.deltaTime;
-            m_animator.SetInteger("AnimIndex", 0);
-            m_animator.SetTrigger("Next");
+            m_animator.SetBool("Walk Forward", false);
 
             if (timer > idleTime)
             {
@@ -49,13 +45,18 @@ public class RabbitMovement : MonoBehaviour
                 moving = true;
             }
         }
+
         else
         {
             timer = 0;
             transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
             transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(Quaternion.LookRotation(targetPosition - transform.position, Vector3.up).eulerAngles), Time.deltaTime * speed);
-            m_animator.SetInteger("AnimIndex", 1);
-            m_animator.SetTrigger("Next");
+
+            //temp stuff (used for better rotation animation, but can't tell the difference between rotating to the left or the right)
+            //Make a: if AngleDifference > 90 degrees or something then, first rotate_animation then movement
+            AngleDifference = Quaternion.Angle(Quaternion.Euler(Quaternion.LookRotation(targetPosition - transform.position, Vector3.up).eulerAngles), transform.rotation);
+
+            m_animator.SetBool("Walk Forward", true);
         }
     }
 
