a4b7f9957539c184fe6b025c59231f9bfa18abfd
Wietse Heijkers <wietseheijkers280@gmail.com>
2019-12-12 13:52:49 +0100

    Main Menu + Pause Menu
    main menu en pause menu

---
+++ UU_GameProject_2019/Assets/UI_scripts_and_prefabs/ButtonNavigation.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonNavigation : MonoBehaviour
+{
+    public string FirstButton;
+
+    public GameObject MenuPanel, OptionsPanel, LevelsPanel;
+
+    
+  /*  void Start()
+    {
+        GameObject.Find(FirstButton).GetComponent<Button>().Select();
+    } */
+
+   /* public void Update()
+    {
+        if (MenuPanel.activeSelf == true)
+        {
+            GameObject.Find(FirstButton).GetComponent<Button>().Select();
+            Debug.Log("MenuPanel is active");
+        }
+        else if (OptionsPanel.activeSelf == true)
+        {
+            GameObject.Find(FirstButton).GetComponent<Button>().Select();
+            Debug.Log("OptionsPanel is active");
+        }
+        else if (LevelsPanel.activeSelf == true)
+        {
+            GameObject.Find(FirstButton).GetComponent<Button>().Select();
+            Debug.Log("LevelsPanel is active");
+        }
+    }*/
+   
+}
---
+++ UU_GameProject_2019/Assets/UI_scripts_and_prefabs/PopUpMenu.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PopUpMenu : MonoBehaviour
+{
+    public static bool IsPaused = false;
+
+    //References to the Pop up menu panel and the OptionsPanel from the main menu
+
+    public GameObject PopUp, OptionsPanel;
+
+    public Button ResumeButton, GoToSettingsButton, ExitGameButton;
+
+    public void Awake()
+    {
+        //On click events for the pop up menu buttons
+
+        ResumeButton.onClick.AddListener(ResumeGame);
+        GoToSettingsButton.onClick.AddListener(OpenSettings);
+        ExitGameButton.onClick.AddListener(ExitGame);
+        
+    }
+
+    void Update()
+    {
+        //check if the escape button is pressed and check if the game is already paused, then resume or pause game accordingly.
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+             if (IsPaused)
+             {
+                 ResumeGame();
+                Debug.Log("You resumed");
+             }
+             else
+             {
+                 PauseGame();
+                 Debug.Log("You paused");
+             }
+        }
+    }
+
+    public void ResumeGame()
+    {
+        PopUp.SetActive(false);
+        Time.timeScale = 1f;
+        IsPaused = false;
+    }
+
+    public void PauseGame()
+    {
+        PopUp.SetActive(true);
+        Time.timeScale = 0f;
+        IsPaused = true;
+    }
+
+    public void OpenSettings()
+    {
+        SceneManager.LoadScene("MenuScene");
+        OptionsPanel.SetActive(true);
+    }
+
+    public void ExitGame()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
