31f1be827da61dcfd2021e9ae6cab71eb5155207
Azat <azatsa@gmail.com>
2019-12-16 19:40:30 +0100

    Added flowers and clouds
    Added flowers and clouds to scenex2y0 and imported flower package.

---
+++ UU_GameProject_2019/Assets/Scripts/CloudMovement.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour
+{
+    public float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, new Vector3(transform.position.x, transform.position.y, transform.position.z - 400), speed*Time.deltaTime);
+    }
+}
