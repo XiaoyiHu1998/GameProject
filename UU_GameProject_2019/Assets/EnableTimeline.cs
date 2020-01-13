b5434139f3c6f43dd25048e058eadffdac465abd
Azat <azatsa@gmail.com>
2020-01-13 20:34:07 +0100

    Added sound tracks and effects

--- UU_GameProject_2019/Assets/EnableTimeline.cs
+++ UU_GameProject_2019/Assets/EnableTimeline.cs
@@ -7,13 +7,10 @@ public class EnableTimeline : MonoBehaviour
 {
     public PlayableDirector playableDirector;
     public GameObject canvas;
-    // Update is called once per frame
+
     public void Play()
     {
-       // if (Input.GetKeyDown(KeyCode.Space))
-
             playableDirector.Play();
             canvas.SetActive(true);
-
     }
 }
