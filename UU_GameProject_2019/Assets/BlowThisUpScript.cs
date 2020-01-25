b6b486ba34effef6641842d10087c75d6e925359
Asehujiko <asehujiko_@hotmail.com>
2020-01-25 21:07:27 +0100

    explodable rock and improved item drops

---
+++ UU_GameProject_2019/Assets/BlowThisUpScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlowThisUpScript : MonoBehaviour, IExplodable
+{
+    public void getExploded()
+    {
+        Destroy(gameObject);
+    }
+}
