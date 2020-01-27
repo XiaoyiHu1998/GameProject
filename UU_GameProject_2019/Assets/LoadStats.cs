e87ec6879c48883e9358e7443dfb24f0223760fd
Azat <azatsa@gmail.com>
2020-01-27 21:26:25 +0100

    fixed scene switching

--- UU_GameProject_2019/Assets/LoadStats.cs
+++ UU_GameProject_2019/Assets/LoadStats.cs
@@ -7,7 +7,10 @@ public class LoadStats : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
+        CharacterController cc = GetComponent<CharacterController>();
+        cc.enabled = false;
         transform.position = PlayerStats.playerPosition;
         transform.rotation = Quaternion.Euler(PlayerStats.playerRotation);
+        cc.enabled = true;
     }
 }
