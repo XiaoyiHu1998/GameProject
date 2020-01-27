a5f4a461c74d2791c11c4413647f774b0e151449
Azat <azatsa@gmail.com>
2020-01-27 15:49:45 +0100

    fix wrong spawning position

--- UU_GameProject_2019/Assets/LoadStats.cs
+++ UU_GameProject_2019/Assets/LoadStats.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class LoadStats : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Awake()
+    void Start()
     {
         transform.position = PlayerStats.playerPosition;
         transform.rotation = Quaternion.Euler(PlayerStats.playerRotation);
