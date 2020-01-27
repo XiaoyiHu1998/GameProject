7a9a69dc0f344d009065cb077c29c3c777f95ff0
Azat <azatsa@gmail.com>
2020-01-27 17:40:53 +0100

    fixed problem in cave

---
+++ UU_GameProject_2019/Assets/ActivatePlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivatePlayer : MonoBehaviour
+{
+    public GameObject player;
+
+    private void OnEnable()
+    {
+        player.SetActive(true);
+    }
+}
