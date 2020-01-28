b6367043d552746234881f265f99f4dff3bd103c
Azat <azatsa@gmail.com>
2020-01-28 12:31:23 +0100

    fixed boss scene

--- UU_GameProject_2019/Assets/Scripts/LoadOnActive.cs
+++ UU_GameProject_2019/Assets/Scripts/LoadOnActive.cs
@@ -6,9 +6,10 @@ public class LoadOnActive : MonoBehaviour
 {
     public Vector3 playerPosition;
     public Vector3 playerRotation;
+    public string sceneName;
 
     public void OnEnable()
     {
-        SceneManager.LoadScene("ShoopBookScene");
+        SceneManager.LoadScene(sceneName: sceneName);
     }
 }
